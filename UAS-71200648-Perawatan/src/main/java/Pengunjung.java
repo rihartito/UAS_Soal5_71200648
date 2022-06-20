4b7a858ada1a1356e50d9d5b98ed8dacd7f3db58
Rihar del tito <78639499+rihartito@users.noreply.github.com>
2022-06-20 18:33:53 +0800

    Delete UAS-71200648-Perawatan directory

--- UAS-71200648-Perawatan/src/main/java/Pengunjung.java
+++
@@ -1,9 +0,0 @@
-public class Pengunjung {
-    private String nama;
-    private int usia;
-    private String alamat;
-    private String penyakit;
-    private int levelPenyakit;
-    private String status;
-
-}
