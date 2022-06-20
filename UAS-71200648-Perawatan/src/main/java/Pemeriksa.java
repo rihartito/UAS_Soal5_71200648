da52a6c7f6af38250b72579058e5cbdb60f600e8
Rihar del tito <78639499+rihartito@users.noreply.github.com>
2022-06-20 18:34:51 +0800

    Add files via upload

---
+++ UAS-71200648-Perawatan/src/main/java/Pemeriksa.java
@@ -0,0 +1,12 @@
+public class Pemeriksa {
+    private String nama;
+    private String spesialis;
+    private String ruangan;
+
+    public void memeriksa(Pengunjung a,Daftar b){
+
+    }
+    public void cekStatus(Pengunjung a){
+
+    }
+}
---
+++ UAS-71200648-Perawatan/src/main/java/Pengunjung.java
@@ -0,0 +1,9 @@
+public class Pengunjung {
+    private String nama;
+    private int usia;
+    private String alamat;
+    private String penyakit;
+    private int levelPenyakit;
+    private String status;
+
+}
